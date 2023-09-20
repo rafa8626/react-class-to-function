61120f1a440b7e644aeba7a01af7456636883a4c
Rafael Miranda <rafa8626@gmail.com>
2023-09-20 13:22:20 -0400

    chore: Updated unit tests to achieve higher coverage

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -16,6 +16,53 @@ describe('conversion with variations', () => {
             })
     );
 
+    it('throws an exception if file does not contain any classes', async () => {
+        const file = resolve(__dirname, 'samples/original/noclass.txt');
+        try {
+            await convertFile(file, true);
+        } catch (e) {
+            expect(e).toHaveProperty('message', `${file} cannot be converted since it is not a Component`);
+        }
+    });
+
+    it('throws an exception if file contains more than 1 class', async () => {
+        const file = resolve(__dirname, 'samples/original/multipleclasses.txt');
+        try {
+            await convertFile(file, true);
+        } catch (e) {
+            expect(e).toHaveProperty('message', `${file} cannot be converted since it contains more than 1 class`);
+        }
+    });
+
+    it('throws an exception if class does not extend a React Component or contains the required `render` method', async () => {
+        let file = resolve(__dirname, 'samples/original/nocomponent1.txt');
+        try {
+            await convertFile(file, true);
+        } catch (e) {
+            expect(e).toHaveProperty('message', 'The class B is not a class component.');
+        }
+
+        file = resolve(__dirname, 'samples/original/nocomponent2.txt');
+        try {
+            await convertFile(file, true);
+        } catch (e) {
+            expect(e).toHaveProperty(
+                'message',
+                'Cannot convert this class since it does not extends a React Component.'
+            );
+        }
+
+        file = resolve(__dirname, 'samples/original/nocomponent3.txt');
+        try {
+            await convertFile(file, true);
+        } catch (e) {
+            expect(e).toHaveProperty(
+                'message',
+                'Cannot convert this class since it does not extends a React Component.'
+            );
+        }
+    });
+
     it('converts a basic class component', async () => {
         const content = await formatTestContent('samples/original/basic1.txt');
         const formatted = await formatExpectedContent('samples/formatted/basic1.txt');
